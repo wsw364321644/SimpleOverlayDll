e03c6969a1c990e58f0af59d0494eb3c9d322399
Savion Wan <364321644@qq.com>
2024-07-11 17:32:52 +0800

    update

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 call .\gen64.bat
-cmake --build .\build64\ --config Release
+cmake --build .\build64\ --config RelWithDebInfo
 call .\gen32.bat
-cmake --build .\build32\ --config Release
+cmake --build .\build32\ --config RelWithDebInfo
 
